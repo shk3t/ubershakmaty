245c0ec1e7fe1b3d8a89a8a96bf093e7b74063b3
shket-228 <daniildudov@gmail.com>
2022-12-05 11:20:10 +0300

    frontend auth doc + service urls fix

--- client/src/services/AuthService.js
+++ client/src/services/AuthService.js
@@ -2,18 +2,18 @@ import {publicConfig, withCredentialsConfig} from "../http"
 
 export default class AuthService {
   static async register(credentials) {
-    const response = await publicConfig.post("/api/register", credentials)
+    const response = await publicConfig.post("/game/register", credentials)
     return response.data
   }
   static async login(credentials) {
-    const response = await publicConfig.post("/api/login", credentials)
+    const response = await publicConfig.post("/game/login", credentials)
     return response.data
   }
   static async logout() {
-    await publicConfig.post("api/logout")
+    await publicConfig.post("game/logout")
   }
   static async refreshTokens() {
-    const response = await withCredentialsConfig.post("/api/tokens/refresh")
+    const response = await withCredentialsConfig.post("/game/tokens/refresh")
     return response.data
   }
 }
