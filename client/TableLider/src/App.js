75b28725a1abdde0655ae15e7713890c7ddb1b7a
Mariia Markelova <m.mashamarkelova@yandex.ru>
2022-12-18 19:24:12 +0300

    Table of lider

---
+++ client/TableLider/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from "./components/Navbar";
+import Table from "./components/Table";
+
+
+
+function App() {
+  return (
+      <div className="App">
+      <Navbar/>
+      <Table/>
+      </div>
+  );
+}
+export default App;
---
+++ client/TableLider/src/constants.js
@@ -0,0 +1,12 @@
+import MainPageGame from "./components/pages/MainPageGame"
+import StatisticPage from "./components/pages/Statistic"
+
+// Pathes
+export const MAIN_PATH = "/main"
+export const STATISTIC_PATH = "/statistic"
+
+// Routes
+export const routes = [
+    { path: MAIN_PATH, Component: MainPageGame},
+    { path: STATISTIC_PATH, Component: StatisticPage}
+]
