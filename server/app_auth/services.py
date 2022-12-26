82ff9e74c5b22629d8053b219d0418517cc3790d
shket-228 <daniildudov@gmail.com>
2022-12-27 00:53:01 +0300

    описание установки бекенда

--- server/app_auth/services.py
+++ server/app_auth/services.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from app_auth.serializers import UserWithTokenSerializer
-from transliterate import translit
 
 
 class AuthService:
@@ -18,10 +17,3 @@ class AuthService:
             httponly=True,
         )
         return response
-
-
-# class TranslitService:
-#     @staticmethod
-#     def append_id(data: dict) -> None:
-#         trans_name = translit(data["name"], "ru", reversed=True)
-#         data["id"] = trans_name.lower().replace(" ", "-").replace("'", "")
