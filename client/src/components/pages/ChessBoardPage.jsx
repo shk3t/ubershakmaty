f884646d78fda1c6a591129b370fe6c69e4a548c
shket-228 <daniildudov@gmail.com>
2022-12-28 15:26:58 +0300

    format

--- client/src/components/pages/ChessBoardPage.jsx
+++ client/src/components/pages/ChessBoardPage.jsx
@@ -1,26 +1,22 @@
-import '../../styles/pages/ChessBoardPageStyles.css'
-import Board from "../Board";
-import Clock from "../Clock";
-import {useState} from "react";
+import "../../styles/pages/ChessBoardPageStyles.css"
+import Board from "../Board"
+import Clock from "../Clock"
+import {useState} from "react"
 
-function ChessBoardPage() {
-  const [whiteMove, setWhiteMove] = useState(true);
+export default function ChessBoardPage() {
+  const [whiteMove, setWhiteMove] = useState(true)
   return (
-    <>
-      <div id="app-wrapper">
-	<div id="game-area">
-          <div id="board-area">
-	    <Board whiteMove={whiteMove} setWhiteMove={setWhiteMove}/>
+    <div id="app-wrapper">
+      <div id="game-area">
+        <div id="board-area">
+          <Board whiteMove={whiteMove} setWhiteMove={setWhiteMove} />
+        </div>
+        <div id="clock-area">
+          <div className="clock-wrapper">
+            <Clock whiteMove={whiteMove} />
           </div>
-          <div id="clock-area">
-            <div className="clock-wrapper">
-	      <Clock whiteMove={whiteMove}/>
-            </div>
-          </div>
-	</div>
+        </div>
       </div>
-    </>
-  );
+    </div>
+  )
 }
-
-export default ChessBoardPage;
