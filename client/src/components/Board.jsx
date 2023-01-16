d564eca7c02cea3258a5cd0d1900a1c085fde51f
shket-228 <daniildudov@gmail.com>
2023-01-16 23:51:31 +0300

    rebase finish

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -54,7 +54,7 @@ export default function BoardComponent() {
                 className={square.possibleMove ? "hint" : null}
               ></div>
             ))}
-          </div>{" "}
+          </div>
         </>
       )}
     </div>
