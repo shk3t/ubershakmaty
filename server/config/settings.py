313e9eebf60cdc8386df0e4fcb49fea0d5e93358
shket-228 <daniildudov@gmail.com>
2023-01-17 23:10:18 +0300

    server cors

--- server/config/settings.py
+++ server/config/settings.py
@@ -111,12 +111,14 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # CORS
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
     "http://localhost:3000",
+    "http://127.0.0.1:8000",
     "http://localhost:8000",
-    "http://localhost:8080",
-)
+]
+CORS_ALLOW_CREDENTIALS = True
+
 
 # REST framework extensions
 REST_FRAMEWORK = {
